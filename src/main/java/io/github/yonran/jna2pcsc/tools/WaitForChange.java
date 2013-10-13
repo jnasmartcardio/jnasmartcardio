c8842794e9c0f52ec5560d9ea7f8a19837d61350
Yonathan Randolph <yonathan@gmail.com>
2013-10-13 02:12:50 -0700

    Add testing tool WaitForChange that exercises waitForChange and list.

---
+++ src/main/java/io/github/yonran/jna2pcsc/tools/WaitForChange.java
@@ -0,0 +1,44 @@
+package io.github.yonran.jna2pcsc.tools;
+import io.github.yonran.jna2pcsc.Smartcardio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.smartcardio.CardException;
+import javax.smartcardio.CardTerminal;
+import javax.smartcardio.CardTerminals;
+import javax.smartcardio.CardTerminals.State;
+import javax.smartcardio.TerminalFactory;
+
+/**
+ * Simple utility to demonstrate {@link CardTerminals#waitForChange()} and
+ * {@link CardTerminals#list(State))}.
+ */
+public class WaitForChange {
+	private static List<String> terminalNames(List<CardTerminal> terminals) {
+		ArrayList<String> r = new ArrayList<String>(terminals.size());
+		for (int i = 0; i < terminals.size(); i++)
+			r.add(terminals.get(i).getName());
+		return r;
+	}
+	public static void main(String[] args) throws CardException {
+		Smartcardio.JnaTerminalFactorySpi context;
+		CardTerminals terminals;
+		if (true) {
+			context = Smartcardio.JnaTerminalFactorySpi.establishContext();
+			terminals = context.engineTerminals();
+		} else {
+			TerminalFactory terminalFactory = TerminalFactory.getDefault();
+			terminals = terminalFactory.terminals();
+		}
+		List<CardTerminal> present = terminals.list(State.CARD_PRESENT);
+		List<CardTerminal> absent = terminals.list(State.CARD_ABSENT);
+		System.out.format("Initial: cards are present in %s; cards are absent from %s%n", terminalNames(present), terminalNames(absent));
+		while (true) {
+			terminals.waitForChange();
+			List<CardTerminal> inserted = terminals.list(State.CARD_INSERTION);
+			List<CardTerminal> removed = terminals.list(State.CARD_REMOVAL);
+			System.out.format("Card inserted in %s; cards removed from %s%n", terminalNames(inserted), terminalNames(removed));
+		}
+	}
+}
