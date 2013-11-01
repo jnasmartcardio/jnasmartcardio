jnasmartcardio/jnasmartcardio

commit 1bd5faa66dc38157c60cfc203ee5a0b8ab4c7a28

Fix function names in Windows.
In WinSCard.h on Windows, some of the functions are actually macros that expand to the ascii or wide version. We use the ascii versions to match pcsclite, which is only ascii.

src/main/java/jnasmartcardio/Winscard.java
@@ -6,10 +6,15 @@ package jnasmartcardio;
 import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import com.sun.jna.FunctionMapper;
 import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.NativeLibrary;
@@ -428,7 +433,18 @@ class Winscard {
 		String libraryName = Platform.isWindows() ? WINDOWS_PATH : Platform.isMac() ? MAC_PATH : PCSC_PATH;
 		WinscardLibrary lib;
 		if (Platform.isWindows() || Platform.isMac()) {
-			lib = (WinscardLibrary) Native.loadLibrary(libraryName, WinscardLibrary.class);
+			HashMap<Object, Object> options = new HashMap<Object, Object>();
+			final Set<String> asciiSuffixNames = new HashSet<String>();
+			asciiSuffixNames.addAll(Arrays.asList("SCardListReaderGroups", "SCardListReaders", "SCardGetStatusChange", "SCardConnect", "SCardStatus"));
+			options.put(Library.OPTION_FUNCTION_MAPPER, new FunctionMapper() {
+				@Override public String getFunctionName(NativeLibrary library, Method method) {
+					String name = method.getName();
+					if (asciiSuffixNames.contains(name))
+						name = name + 'A';
+					return name;
+				}
+			});
+			lib = (WinscardLibrary) Native.loadLibrary(libraryName, WinscardLibrary.class, options);
 		} else {
 			PcscLiteLibrary linuxPcscLib = (PcscLiteLibrary) Native.loadLibrary(libraryName, PcscLiteLibrary.class);
 			lib = new PcscLiteAdapter(linuxPcscLib);

commit d1a5ae7a97b853eb9a10093c6bde29fe053aed31

Fix bug for OS X introduced by 1bd5faa6 (Fix function names in Windows.)
Remove the function mapper when loading the PCSC library on OS X. The ASCII symbol mapper is only needed on Windows and was accidentally also added to Mac.

src/main/java/jnasmartcardio/Winscard.java
@@ -447,8 +447,8 @@ class Winscard {
 	public static WinscardLibInfo openLib() {
 		String libraryName = Platform.isWindows() ? WINDOWS_PATH : Platform.isMac() ? MAC_PATH : PCSC_PATH;
 		WinscardLibrary lib;
-		if (Platform.isWindows() || Platform.isMac()) {
-			HashMap<Object, Object> options = new HashMap<Object, Object>();
+		HashMap<Object, Object> options = new HashMap<Object, Object>();
+		if (Platform.isWindows()) {
 			final Set<String> asciiSuffixNames = new HashSet<String>();
 			asciiSuffixNames.addAll(Arrays.asList("SCardListReaderGroups", "SCardListReaders", "SCardGetStatusChange", "SCardConnect", "SCardStatus"));
 			options.put(Library.OPTION_FUNCTION_MAPPER, new FunctionMapper() {
@@ -459,9 +459,11 @@ class Winscard {
 					return name;
 				}
 			});
+		}
+		if (Platform.isWindows() || Platform.isMac()) {
 			lib = (WinscardLibrary) Native.loadLibrary(libraryName, WinscardLibrary.class, options);
 		} else {
-			PcscLiteLibrary linuxPcscLib = (PcscLiteLibrary) Native.loadLibrary(libraryName, PcscLiteLibrary.class);
+			PcscLiteLibrary linuxPcscLib = (PcscLiteLibrary) Native.loadLibrary(libraryName, PcscLiteLibrary.class, options);
 			lib = new PcscLiteAdapter(linuxPcscLib);
 		}
 		Class<? extends SCardReaderState> scardReaderStateClass;